05c9b372a6ad7931ee914d07e34aa83fb5e38e28
proc-7b2 <dodomartbusiness@gmail.com>
2026-02-02 09:59:30 +0000

    Resolve README conflict and apply local changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
-import streamlit as st
+# example/st_app.py
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+data = conn.read(worksheet="Testing DATA.1")
+st.dataframe(data)
